0b9041ac1ce55165cc51091ac76fcb694f615707
Kris De Winter <kdw2060@users.noreply.github.com>
2018-09-05 16:41:40 +0200

    testing openode deploy

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-//Importing libraries
-const express = require('express');
+//Import and config libraries
+var express = require('express');
 var bodyparser = require('body-parser');
+var http = require('http');
 var request = require('request');
-const app = express();
+var app = express();
 app.use(express.static('client'));
+var port = normalizePort(process.env.PORT || '3002');
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 var ICAL = require('ical.js');
 const icalGenerator = require('ical-generator');
 
@@ -208,4 +215,64 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.listen(process.env.PORT || 8888);
+//app.listen(port);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
