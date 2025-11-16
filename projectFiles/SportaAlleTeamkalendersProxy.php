47faa382b8bbd3cd913eaecebacf344c3237b07a
Kris De Winter <kdw2060@users.noreply.github.com>
2025-11-16 18:43:58 +0100

    Deleting old version

--- projectFiles/SportaAlleTeamkalendersProxy.php
+++
@@ -1,4 +0,0 @@
-<?php header('Access-Control-Allow-Origin: http://www.ostaberchem.be', false);
-header('Content-Type: application/json');
-?>
-<?php echo file_get_contents('https://mijnbeheer.sportafederatie.be/competities/publiek/schema/7/json?organisation=1684'); ?>
--- projectFiles/SportaCompetitiestandenProxy.php
+++
@@ -1,4 +0,0 @@
-<?php header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json');
-?>
-<?php echo file_get_contents('https://mijnbeheer.sportafederatie.be/competities/publiek/rangschikking/7/json?type=league'); ?>
